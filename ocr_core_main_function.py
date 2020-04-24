0fb464dc313866671967b6988a164f90fcb9e82e
Jenny Dcruz <jendcruz23@gmail.com>
2020-04-24 17:33:08 +0530

    new accuracy images

--- ocr_core_main_function.py
+++ ocr_core_main_function.py
@@ -1,11 +1,17 @@
 import pytesseract as tess
+import string
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
-def ocr_core(filename):
-    img = Image.open(filename)
-    text= tess.image_to_string(img)
-    return(text)
+img = Image.open('accuracy/images/New folder/1.jpg')
+text= tess.image_to_string(img)
+# print(text)
 
-print(ocr_core('accuracy/images/theatre.jpg'))
-
+text = text.lower().replace(',','').replace('_','').replace(':','').replace('=','').replace('(','').replace(')','').replace('"','').replace('|','').replace('#','').replace('$','').replace('&','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
+print(text)
+total = 0
+ 
+for i in text:
+    total = total + 1
+ 
+print("Total Number of Characters in this String = ", total)
