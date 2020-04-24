7a5d1a6092d7dbf25e6a56661c98d1e8cd2c0b93
Jenny Dcruz <jendcruz23@gmail.com>
2020-04-24 17:45:47 +0530

    charcount.py updated

--- ocr_core_main_function.py
+++ ocr_core_main_function.py
@@ -3,11 +3,11 @@ import string
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
-img = Image.open('accuracy/images/New folder/1.jpg')
+img = Image.open('accuracy/images/New folder/14.jpg')
 text= tess.image_to_string(img)
 # print(text)
 
-text = text.lower().replace(',','').replace('_','').replace(':','').replace('=','').replace('(','').replace(')','').replace('"','').replace('|','').replace('#','').replace('$','').replace('&','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
+text = text.lower().replace(',','').replace('\n',' ').replace('_','').replace(':','').replace('=','').replace('(','').replace(')','').replace('"','').replace('|','').replace('#','').replace('$','').replace('&','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
 print(text)
 total = 0
  
