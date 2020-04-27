d84df65d40087cb0b92121eef7b420dc163abfb7
Jenny Dcruz <jendcruz23@gmail.com>
2020-04-27 12:27:40 +0530

    debuging

--- ocr_core.py
+++ ocr_core.py
@@ -9,15 +9,14 @@ import string
 
 def ocr_core(filename):
     extractedInformation = tess.pytesseract.image_to_string(Image.open(filename))
-
     text = extractedInformation.lower().replace(',','').replace(' ','').replace('_','').replace(':','').replace('=','').replace('(','').replace(')','').replace('"','').replace('|','').replace('#','').replace('$','').replace('&','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
     text = text.translate({ord(c): None for c in string.whitespace})
+    print(text)
+
     with open('tess.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             for col in row:
                 if col==text:
                     return("\nName of the Book: "+row[1]+"Author: "+row[2])
-                else:
-                    return(tess.pytesseract.image_to_string(Image.open(filename)))
-
+    
